14faee67bbacd8149092c0a2d5d6c91f3c29116a
Nisha <hayaazashraf@gmail.com>
2024-10-27 09:18:09 -0700

    first commit

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,71 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            I'm a Full Stack Developer,
+
+            Developed full-Stack applications across various platforms using latest industry-adopted technologies and frameworks
+            Played a key role in the development,improvements,and operation of web-based software
+            
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+   
+    {
+      title: "2011 Bachelors Degree",
+      content: (
+        <>
+          <p>
+            Completed Bachelors Degree in Justice Basheer Ahmed Sayeed College in Major of Info-Sys Management
+            
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "2017 Backend role in HDFC Bank",
+      content: (
+        <>
+          <p>
+            Advises customers on the completion of loan applications.Collection of all relevant
+            documents sent through different channels i.e. Branch DSA & DST.
+            
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2020 JIO INFOCOMM IN RELIANCE RETAIL LIMITED",
+      content: (
+        <>
+          <p>
+            Handling customer qureies and complaints of Jio-Fi.Training attended conducted
+            by Senior officials and central trainig team and also plays a Team-Leader in Reliance Digital Limited
+            
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2024 Congruence Holdings",
+      content: (
+        <>
+          <p>
+            Plays a Role in Customer Support Executive maintaining a proper record of the customers including the purchases made by the customer, billings, contact information, and other relevant details. 
+            
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
